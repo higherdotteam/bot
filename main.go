a9d963484f1c113d8e4296a662e595f87ba873b5
A Arrow <oneone@gmail.com>
2017-01-04 05:10:34 +0000

    m

--- main.go
+++ main.go
@@ -9,8 +9,18 @@ import "encoding/json"
 import "os"
 
 type SlackResponse struct {
-	// "type": "url_verification", "token": "QTT8T3f8VFmZ2MvH4m3jLBCh", "challenge": "q2TjwXAtQrPYt9ts3pxAmg3ryaNV0Cgo3OS9xJFlkghzuRkawyIe"
 	Type      string `json:"type"`
+	Token     string `json:"token"`
+	Challenge string `json:"challenge"`
+}
+
+type SlackAccess struct {
+	Type      string `json:"access_token"`
+	Type      string `json:"user_id"`
+	Type      string `json:"team_name"`
+	Type      string `json:"team_id"`
+	Type      string `json:""`
+	Type      string `json:"access_token"`
 	Token     string `json:"token"`
 	Challenge string `json:"challenge"`
 }
@@ -32,9 +42,29 @@ func main() {
 		body, _ := ioutil.ReadAll(r.Body)
 		fmt.Println(string(body))
 
+		/* {"ok":true,"access_token":"6f","scope":"identify,bot","user_id":"U1","team_name":"higher.team","team_id":"TL","bot":{"bot_user_id":"UP","bot_access_token":"zg"}} */
+
 		c.Redirect(http.StatusMovedPermanently, "http://higher.team/")
 	})
 	r.POST("/slack", func(c *gin.Context) {
+
+		/* {"token":"h","team_id":"T035N23CL","api_app_id":"A3M072NLA",
+		"event":{"type":"message","user":"U035LF6C1","text":"fwefef",
+		         "ts":"1483496552.000007","channel":"D3MN1KCHM",
+						 "event_ts":"1483496552.000007"},
+		"type":"event_callback","authed_users":["U3LV4ST2P"]} */
+
+		body := c.Request.Body
+		x, _ := ioutil.ReadAll(body)
+
+		result := SlackResponse{}
+		json.Unmarshal(x, &result)
+
+		fmt.Printf("%v", result)
+
+		c.String(200, result.Challenge)
+	})
+	r.POST("/slack/challenge", func(c *gin.Context) {
 		body := c.Request.Body
 		x, _ := ioutil.ReadAll(body)
 
