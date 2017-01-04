15b527e23e4a5f4c3421041c1a2b2b5f1ddd10bf
A Arrow <oneone@gmail.com>
2017-01-04 00:21:57 +0000

    m

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import "github.com/gin-gonic/gin"
-import "net/http"
+
+//import "net/http"
 import "fmt"
 
 func main() {
@@ -10,19 +11,15 @@ func main() {
 	r.LoadHTMLGlob("templates/*.tmpl")
 	r.Static("/assets", "./assets")
 
-	r.POST("/", func(c *gin.Context) {
-fmt.Println("111111")
+	r.GET("/", func(c *gin.Context) {
 		data := gin.H{"nav_dash": true}
 		c.HTML(200, "index.tmpl", data)
 	})
-	r.GET("/list", func(c *gin.Context) {
-		data := gin.H{"nav_dash": true}
-		c.HTML(200, "list.tmpl", data)
-	})
-	r.POST("/order", func(c *gin.Context) {
-		//_ := c.PostForm("stripeToken")
-		c.Redirect(http.StatusMovedPermanently, "http://cordbouquet.tumblr.com/post/143915124906/thank-you")
+	r.POST("/slack", func(c *gin.Context) {
+		//data := gin.H{"nav_dash": true}
+		c.String(200, "wefwef")
 	})
 
-	r.RunTLS(":443", "/etc/letsencrypt/live/higher.team/fullchain.pem", "/etc/letsencrypt/live/higher.team/privkey.pem")
+	//r.RunTLS(":443", "/etc/letsencrypt/live/higher.team/fullchain.pem", "/etc/letsencrypt/live/higher.team/privkey.pem")
+	r.Run()
 }
